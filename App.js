f87641ac975319e5ffaf9be1d3485890ad8ec3b9
zearkiatos <caprilespe@outlook.com>
2021-11-28 00:11:56 -0300

    useCallback

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
 import Counter from "./src/components/Counter";
 import Users from "./src/components/Users";
 import UseReducer from "./src/components/UseReducer";
 import Memo from "./src/components/Memo";
+import CallBack from "./src/components/CallBack";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Counter />
-      <Users />
-      <UseReducer />
-      <Memo />
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Counter />
+        <Users />
+        <UseReducer />
+        <Memo />
+        <CallBack />
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
 
